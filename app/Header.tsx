d70a9cbaf32de665ee6dcbfe4acbcd21d4139514
elice63 <elice63@elice63ui-iMac.local>
2023-08-27 00:19:17 +0900

    add user component

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,12 +1,14 @@
 import styles from './header.module.css'
+import Image from 'next/image'
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <h1>Study with AI</h1>
-      <nav>
-        <span>Menu1</span>
-      </nav>
+      <div className={styles.user}>
+        <span className={styles.userName}>User42</span>
+        <Image src={"https://ui-avatars.com/api/?name=John+Doe"} alt="user icon" width={32} height={32} unoptimized={true} className={styles.userImage}/>
+      </div>
     </header>
   )
 }
