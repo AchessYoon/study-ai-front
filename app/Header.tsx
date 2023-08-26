562413c9a83919914d47096d7e8ccc0ac505b195
elice63 <elice63@elice63ui-iMac.local>
2023-08-27 03:49:33 +0900

    change service name

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>Study with AI</h1>
+      <h1>CogniCalBook</h1>
       <div className={styles.user}>
         <span className={styles.userName}>User42</span>
         <Image src={"https://ui-avatars.com/api/?name=John+Doe"} alt="user icon" width={32} height={32} unoptimized={true} className={styles.userImage}/>
