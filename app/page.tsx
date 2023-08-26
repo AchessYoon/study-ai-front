bcb31c2b6d0615419dd1549734b38a4f6338dfb2
elice63 <elice63@elice63ui-iMac.local>
2023-08-27 04:29:37 +0900

    implement upload new button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import styles from './page.module.css'
 import Hint from './Hint'
 import AnswerInput from './AnswerInput'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUpload } from '@fortawesome/free-solid-svg-icons'
 
 const problem = "철수와 영희는 자신의 집에서 상점을 향해 동시에 출발하였다. 영희의 집은 상점에서 6km떨어져 있고, 철수가 영희의 2배의 속력으로 이동하여 출발한 지 30분 만에 상점이 2km남은 지접에서 영희를 추월했을때, 철수의 속력은?(단, 영희의 집은 철수의 집과 상점사이에 있다.)"
 
@@ -10,7 +12,13 @@ export default function Home() {
       <div className={styles.leftSection}>
         <div className={styles.problem}>{problem}</div>
         <textarea className={styles.solving}/>
-        <AnswerInput />
+        <div className={styles.bottomSection}>
+          <AnswerInput />
+          <button className={styles.uploadNew}>
+            <FontAwesomeIcon icon={faUpload}/>
+            새 문제
+          </button>
+        </div>
       </div>
       <div className={styles.rightSection}>
         <Hint title="힌트1"/>
