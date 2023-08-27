fdc2eb6527030cc9dcb9fbfeabb6f1d841421124
elice63 <elice63@elice63ui-iMac.local>
2023-08-27 10:57:02 +0900

    fix async

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-async-client-component */
 "use client"
 
 import styles from './page.module.css'
@@ -14,7 +13,7 @@ import { useState } from 'react'
 const problem = "철수와 영희는 자신의 집에서 상점을 향해 동시에 출발하였다. 영희의 집은 상점에서 6km떨어져 있고, 철수가 영희의 2배의 속력으로 이동하여 출발한 지 30분 만에 상점이 2km남은 지접에서 영희를 추월했을때, 철수의 속력은?(단, 영희의 집은 철수의 집과 상점사이에 있다.)"
 const hint = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
 
-export default async function Home() {
+export default function Home() {
   const {fileName, fileEncodedBase64} = useGetFile()
   const router = useRouter()
   const [resJson, setResJson] = useState<{ hint1: string, hint2: string, hint3: string, answer: string, problemText: string}|null>()
@@ -22,16 +21,16 @@ export default async function Home() {
   if(fileName) {
     console.log(JSON.stringify({filename: fileName, fileEncodedBase64}))
     try {
-      const res = await fetch("http://15.164.81.24:8080/api/sample",{
+
+      
+      fetch("http://15.164.81.24:8080/api/sample",{
         method: "POST",
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({filename: fileName, fileEncodedBase64})
-      })
-      const j = await res.json()
-      setResJson(j)
+      }).then((res)=>{res.json().then((j)=>{setResJson(j)})})
     } catch (e) {
       console.log(e)
       // router.push("/upload")
