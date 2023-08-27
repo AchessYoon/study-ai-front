4fc1517618bebb2315b446b1f32af2c75c0e122e
elice63 <elice63@elice63ui-iMac.local>
2023-08-27 11:17:12 +0900

    add image type inpput

--- app/upload/DragDrop.tsx
+++ app/upload/DragDrop.tsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 import styles from './dragDrop.module.css'
 import {useSetFile} from '../useFileContext'
@@ -8,26 +8,33 @@ import { useRouter } from "next/navigation";
 const fileTypes = ["JPG", "PNG", "GIF"];
 
 export default function DragDrop() {
-    const [file, setFile] = useState(null);
+    const [file, setFile] = useState<any>(null);
+    const [fileType, setFileType] = useState<boolean>(false);
     const setContextFile = useSetFile()
     const router = useRouter()
-    const handleChange = (file: any) => {
-        setFile(file);
+    const handleChange = useCallback((f: any) => {
+        setFile(f);
         var reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = function () {
             setContextFile({fileName: file.name, fileEncodedBase64: reader.result});
             router.push("/");
         };
-    };
+    },[file, router, setContextFile]);
+    const handleTypeChange = useCallback((e: any) => {
+        setFileType(e.target.checked);
+    }, [setFileType])
+
     useEffect(()=>{
         (document.getElementsByTagName("label")[0]?.children[2].children[1] as any)?.style.setProperty("max-width", "none")
     })
+
     return (
-        <>
         <div className={styles.dragDrop}>
-        <FileUploader handleChange={handleChange} name="file" types={fileTypes} multiple={false}/>
+            <FileUploader handleChange={handleChange} name="file" types={fileTypes} multiple={false}/>
+            <label>
+                <input role="switch" type="checkbox" onChange={e => handleTypeChange(e)}/>
+            </label>
         </div>
-        </>
     );
 }
